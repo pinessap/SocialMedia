da93d8b2947b7f284a5c386ce3460a6df05c938b
Natalia <101643603+natialia@users.noreply.github.com>
2022-04-21 12:34:09 +0200

    Update navbar.php

--- SocialMedia/navbar.php
+++ SocialMedia/navbar.php
@@ -56,10 +56,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href=".-.php">blob</a>
+              <a class="nav-link active" aria-current="page" href="post.php">Posts</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="post.php">Posts</a>
+              <a class="nav-link active" aria-current="page" href=".-.php">blob</a>
             </li>
           </ul>
         </div>
