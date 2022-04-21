69fb584c3072dfc8316244e609a9294fc914dd11
Natalia <101643603+natialia@users.noreply.github.com>
2022-04-21 12:22:43 +0200

    Update function.php

--- SocialMedia/php/function.php
+++ SocialMedia/php/function.php
@@ -1,4 +1,5 @@
 <?php
+
     function emptyInputSignup($salutation, $email, $username, $pwd, $passwordRepeat, $name, $surname) {
 
         if (empty($salutation) ||empty($email) || empty($username) || empty($pwd) || empty($passwordRepeat) || empty($name) ||empty($surname)) {
@@ -81,3 +82,46 @@
             header("Location: ../register.php?error=none");
             exit();
         }
+
+    function makeThumbnails($updir, $img, $id, $MaxWe=720, $MaxHe=480){
+        $arr_image_details = getimagesize($img); 
+        $width = $arr_image_details[0];
+        $height = $arr_image_details[1];        
+        
+        $percent = 100;    
+        if($width > $MaxWe) $percent = floor(($MaxWe * 100) / $width);    
+        
+            if(floor(($height * $percent)/100)>$MaxHe)  
+            $percent = (($MaxHe * 100) / $height);
+        
+            if($width > $height) {
+                $newWidth=$MaxWe;
+                $newHeight=round(($height*$percent)/100);
+            }else{
+                $newWidth=round(($width*$percent)/100);
+                $newHeight=$MaxHe;
+            }
+        
+            //Can currently create thumbnails from 3 filetypes, even though only 2 are permitted
+            if ($arr_image_details[2] == 1) {
+                $imgt = "ImageGIF";
+                $imgcreatefrom = "ImageCreateFromGIF";
+            }
+            if ($arr_image_details[2] == 2) {
+                $imgt = "ImageJPEG";
+                $imgcreatefrom = "ImageCreateFromJPEG";
+            }
+            if ($arr_image_details[2] == 3) {
+                $imgt = "ImagePNG";
+                $imgcreatefrom = "ImageCreateFromPNG";
+            }
+        
+            if ($imgt) {
+                $old_image = $imgcreatefrom($img);
+                $new_image = imagecreatetruecolor($newWidth, $newHeight);
+                imagecopyresized($new_image, $old_image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+        
+               $imgt($new_image, $updir."/".$id."_t.jpg");
+                return;    
+            }
+        }        
