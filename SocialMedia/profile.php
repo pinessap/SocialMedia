184cb49f77f37c594d731a8873ced22311c91ca8
pinessap <inespetrusic@gmail.com>
2022-04-25 20:03:03 +0200

    Update profile.php

--- SocialMedia/profile.php
+++ SocialMedia/profile.php
@@ -10,116 +10,120 @@
         $row = $query->fetch_array();
 ?> 
 
-
-
 <body>
-	<section class="container">
-
-		<div class="imgcontainer">
-		<img src="img\avatar.png" alt="Avatar" class="avatar">
-		</div>
-        <h4>My Profile</h4>
-         <hr>
-           <div>
-                <p>Saluation: <?php echo htmlspecialchars($row['salutation']);  ?></p>
-            </div>
-            <div>
-                <p>Name: <?php echo htmlspecialchars($row['name']);  ?></p>
-            </div>
-            <div>
-                <p>Surname: <?php echo htmlspecialchars($row['surname']);  ?></p>
-            </div> 
-            <div>
-                <p>Username: <?php echo htmlspecialchars($row['username']);  ?></p>
-            </div>
-            <div>
-                <p>Email: <?php echo htmlspecialchars($row['email']);  ?></p>
-            </div>
-            <hr>	
-        </div>
-
-	    <form action=" " method="post">
-            <a href="profile.php?changeData"><button class="ticketbtn" name='change-submit' type="button">Change</button></a>	
-	    </form>
-	</section>
+    <br>
+        
 
     <?php
+    if(isset($_GET['mypage'])){
+        include 'php/newpost.php';
+        include 'php/viewposts.php';
+    }
+    
+
+    if(isset($_GET['edit'])){
+        include 'edit_profile.php';
+    }
+
+    if(isset($_GET['changepicture'])){
+           include 'php/profilepicture.php';
+    }
+    
  
 
-        if(isset($_GET['changeData'])){
-            include 'php/changeData.php';
+    if(isset($_GET['changeData'])){
+        include 'php/changeData.php';
+    }
+
+    if(isset($_GET['success'])){
+        if($_GET["success"] == "pic"){?>
+            <br>
+            <div class="container">                                                         
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Profile Picture was successful!</h4>
+                </div>
+            </div><br><?php
         }
-        if(isset($_GET['success'])){?>
+        else{?>
             <br>
             <div class="container">                                                                 <!--erfolgreich--> 
                 <div class="alert alert-secondary" role="alert">
                     <h4 style="color:rgb(72,72,72);">Changing Data was successful!</h4>
                 </div>
+            </div><br><?php 
+        }
+    }
+    if(isset($_GET["error"])) {
+        if($_GET["error"] == "username"){?>
+            <br>
+            <div class="container">                                                         
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Username was unsuccessful!</h4>
+                </div>
             </div><br><?php
         }
-        if(isset($_GET["error"])) {
-            if($_GET["error"] == "username"){?>
-                <br>
-                <div class="container">                                                         
-                    <div class="alert alert-secondary" role="alert">
-                        <h4 style="color:rgb(72,72,72);">Changing Username was unsuccessful!</h4>
-                    </div>
-                </div><br><?php
-            }
-            if($_GET["error"] == "email"){?>
-                <br>
-                <div class="container">                                                            
-                    <div class="alert alert-secondary" role="alert">
-                        <h4 style="color:rgb(72,72,72);">Changing Email was unsuccessful!</h4>
-                    </div>
-                </div><br><?php
-            }
-            if($_GET["error"] == "salutation"){?>
-                <br>
-                <div class="container">                                                           
-                    <div class="alert alert-secondary" role="alert">
-                        <h4 style="color:rgb(72,72,72);">Changing Salutation was unsuccessful!</h4>
-                    </div>
-                </div><br><?php
-            }
-            if($_GET["error"] == "name"){?>
-                <br>
-                <div class="container">                                                        
-                    <div class="alert alert-secondary" role="alert">
-                        <h4 style="color:rgb(72,72,72);">Changing Name was unsuccessful!</h4>
-                    </div>
-                </div><br><?php
-            }
-            if($_GET["error"] == "surname"){?>
-                <br>
-                <div class="container">                                                           
-                    <div class="alert alert-secondary" role="alert">
-                        <h4 style="color:rgb(72,72,72);">Changing Surname was unsuccessful!</h4>
-                    </div>
-                </div><br><?php
-            }
-            if($_GET["error"] == "password"){?>
-                <br>
-                <div class="container">                                                              
-                    <div class="alert alert-secondary" role="alert">
-                        <h4 style="color:rgb(72,72,72);">Changing Password was unsuccessful!</h4>
-                    </div>
-                </div><br><?php
-            }
-            if($_GET["error"] == "edit"){?>
-                <br>
-                <div class="container">                                                               
-                    <div class="alert alert-secondary" role="alert">
-                        <h4 style="color:rgb(72,72,72);">Changing Data was unsuccessful!</h4>
-                    </div>
-                </div><br><?php
-            }
+        if($_GET["error"] == "email"){?>
+            <br>
+            <div class="container">                                                            
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Email was unsuccessful!</h4>
+                </div>
+            </div><br><?php
         }
-        
-        
+        if($_GET["error"] == "salutation"){?>
+            <br>
+            <div class="container">                                                           
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Salutation was unsuccessful!</h4>
+                </div>
+            </div><br><?php
+        }
+        if($_GET["error"] == "name"){?>
+            <br>
+            <div class="container">                                                        
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Name was unsuccessful!</h4>
+                </div>
+            </div><br><?php
+        }
+        if($_GET["error"] == "surname"){?>
+            <br>
+            <div class="container">                                                           
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Surname was unsuccessful!</h4>
+                </div>
+            </div><br><?php
+        }
+        if($_GET["error"] == "password"){?>
+            <br>
+            <div class="container">                                                              
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Password was unsuccessful!</h4>
+                </div>
+            </div><br><?php
+        }
+        if($_GET["error"] == "edit"){?>
+            <br>
+            <div class="container">                                                               
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Data was unsuccessful!</h4>
+                </div>
+            </div><br><?php
+        }
+        if($_GET["error"] == "pic"){?>
+            <br>
+            <div class="container">                                                              
+                <div class="alert alert-secondary" role="alert">
+                    <h4 style="color:rgb(72,72,72);">Changing Profile Picture was unsuccessful!</h4>
+                </div>
+            </div><br><?php
+        }
+    }
     ?>
+
 </body>
 
-<?php }
-    require 'footer.php';?>
+    <?php }
+        require 'footer.php';
+    ?>
 </html>
