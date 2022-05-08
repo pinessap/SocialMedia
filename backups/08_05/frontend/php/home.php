e09b455b151c41c4ba5ed69695a3e6a60298fd66
pinessap <inespetrusic@gmail.com>
2022-05-08 20:44:46 +0200

    Add files via upload

---
+++ backups/08_05/frontend/php/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        //session_start();
+        require '../backend/config/db_conn.php';
+        include 'header.php'
+    ?>
+
+    <script src="../js/homescript.js" type="text/javascript"></script>
+    <title>Home</title>
+</head>
+<body>
+    
+    <?php
+       // include 'navbar.php';
+        include 'profile/newPost.php';
+        $session_uid = $_SESSION['id'];
+    ?>
+    <p id="session_uid" hidden><?php echo $session_uid; ?></p>
+
+    <div class="container" id="homepage">
+        
+    </div>
+    
+    
+</body>
+</html>
+
